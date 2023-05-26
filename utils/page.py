b8ab37f8aa12acff44b37f910fc0a8fbcdd19c6d
Will <wuyi5160@gmail.com>
2023-05-26 19:16:48 +0800

    expand rows per page to 80 for DN module

--- utils/page.py
+++ utils/page.py
@@ -3,7 +3,7 @@ from rest_framework.utils.urls import replace_query_param, remove_query_param
 from rest_framework.exceptions import APIException
 
 class MyPageNumberPagination(PageNumberPagination):
-    page_size = 60
+    page_size = 80
     page_size_query_param = "max_page"
     max_page_size = 1000
     page_query_param = 'page'
